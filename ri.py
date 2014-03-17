a6c7c8deb264c8d12480d6e9ecd1d7046e39db3c
Cristian Capdevila <ccapdevi@utk.edu>
2014-03-17 18:36:37 -0500

    initial attempt to push files to hadoop fs

--- ri.py
+++ ri.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #import os
 import sys
+import subprocess as sp
 
 
 def usage():
@@ -14,6 +15,7 @@ def file_number(in_filename):
     outfile = None
     infile = None
     line_no = 1
+
     try:
         # open our input file and output file
         infile = open(in_filename, 'r')
@@ -28,12 +30,10 @@ def file_number(in_filename):
 
     except IOError as e:
         sys.exit('Error opening \'' + e.filename + '\': ' + e.strerror)
+
     finally:
         if infile:
             infile.close()
-        if outfile:
-            outfile.close()
-
 
 if __name__ == '__main__':
     # print usage information/check arg count
@@ -45,5 +45,17 @@ if __name__ == '__main__':
         else:
             sys.exit(1)
 
+    hdfs_home = sys.argv[2]
     # create a new file with line numbers
     datafile = file_number(sys.argv[1])
+
+    # copy the new file to the hdfs file system
+    put_cmd = 'hadoop fs -put ' + datafile.name + ' ' + hdfs_home
+    print(put_cmd)
+    p = sp.Popen(put_cmd, shell=True, stderr=sp.PIPE)
+    p.wait()
+    if p.returncode is not 0:
+        print('An error occurred copying data to hdfs filesystem:\n\n')
+        for line in p.stderr:
+            print(line)
+        sys.exit(1)
