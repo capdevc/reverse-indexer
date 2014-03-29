e82c10f53e45bd10b1ebbd4fc0bcee5b1e456c46
Cristian Capdevila <ccapdevi@utk.edu>
2014-03-29 00:15:01 -0400

    removed uneeded parameter

--- runri.py
+++ runri.py
@@ -14,7 +14,7 @@ def subp_cmd(cmd, err_msg):
         sys.exit(1)
 
 
-def runri(infiles, outfile, hdfs_home, jar, threshold, hadoop_path):
+def runri(infiles, outfile, jar, threshold, hadoop_path):
     # add the hadoop path if needed
     hadoop_cmd = 'hadoop '
     if hadoop_path:
@@ -24,20 +24,20 @@ def runri(infiles, outfile, hdfs_home, jar, threshold, hadoop_path):
     # build a string of inputfiles + hdfs path
     hdfs_infstr = ''
     for infile in infiles:
-        infstr += ' {}/{}'.format(hdfs_home, infile)
+        infstr += ' /{}'.format(infile)
 
-    subp_cmd('{} fs -put {} {}'.format(hadoop_cmd, infstr, hdfs_home),
+    subp_cmd('{} fs -put {} /'.format(hadoop_cmd, infstr),
              'An error occurred copying the input files to hdfs.\n')
 
     # run the jar file
     cmd = '{} jar {} ReverseIndexer'.format(hadoop_cmd, jar)
     if threshold != 0:
         cmd += ' -D threshold={}'.format(threshold)
-    cmd += ' {}/output {}'.format(hdfs_home, hdfs_infstr)
+    cmd += ' /output {}'.format(hdfs_infstr)
     subp_cmd(cmd, 'An error occurred running the hadoop job:\n')
 
     # copy the output files
-    subp_cmd('{} fs -get {}/output/part-r-00000 {}'.format(hadoop_cmd, hdfs_home, outfile),
+    subp_cmd('{} fs -get /output/part-r-00000 {}'.format(hadoop_cmd, outfile),
              'An error ocurred retrieving output file from hdfs')
 
     # erase the input file from the hdfs system
@@ -45,15 +45,13 @@ def runri(infiles, outfile, hdfs_home, jar, threshold, hadoop_path):
              'An error occurred rming temp file from the hdfs filesystem:\n')
 
     # erase the output files from the hdfs system
-    subp_cmd('{} fs -rmr {}/output'.format(hadoop_cmd, hdfs_home),
+    subp_cmd('{} fs -rmr /output'.format(hadoop_cmd),
              'An error occurred rming output file from the hdfs filesystem:\n')
-
 
 
 if __name__ == '__main__':
     # Do some command line parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument('hdfs_home', help='Path to hdfs home')
     parser.add_argument('outfile', help='Name of the output file')
     parser.add_argument('infile', nargs='+',
                         help='Names of the text files to be numbered')
@@ -62,7 +60,7 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--hadoop', default=None, metavar='PATH',
                         help='Path to the hadoop binary (default: "")')
     parser.add_argument('-t', '--threshold', type=int, default=0,
-                        help='Maximum appearances before a word is considered a stop word')
+                        help='Max appearances before a word is considered a stop word')
     args = parser.parse_args()
 
-    runri(args.infile, args.outfile, args.hdfs_home, args.jar, args.threshold, args.hadoop)
+    runri(args.infile, args.outfile, args.jar, args.threshold, args.hadoop)
